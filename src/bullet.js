721dd9ca0b834a089e0ccdb1e3119611b3b2bbb8
v_ztizhang <v_ztizhang@tencent.com>
2018-12-03 16:12:09 +0800

    completed

---
+++ src/bullet.js
@@ -0,0 +1,77 @@
+export default class bullet {
+    constructor (parent, water, type) {
+        this.parent = parent;
+        this.type = type;
+        this.bullet = document.createElement('div');
+        this.bullet.classList.add('bullet');
+        this.bullet.classList.add(this.type);
+        this.isDestoryed = false;
+
+        switch (type) {
+            case 'left':
+                this.left = water.left - 50;
+                this.top = water.top;
+                break;
+            case 'right':
+                this.left = water.left + 50;
+                this.top = water.top;
+                break;
+            case 'top':
+                this.left = water.left;
+                this.top = water.top - 50;
+                break;
+            case 'bottom':
+                this.left = water.left;
+                this.top = water.top + 50;
+                break;
+        }
+        
+        this.bullet.style.left = this.left + 'px';
+        this.bullet.style.top = this.top + 'px';
+        parent.wrap.appendChild(this.bullet);
+        this.move();
+    }
+
+    move () {
+        const wrap = this.parent.wrap;
+        this.run = setInterval (() => {
+            switch (this.type) {
+                case 'left':
+                    this.left = this.left - 5;
+                    break;
+                case 'right':
+                    this.left = this.left + 5;
+                    break;
+                case 'top':
+                    this.top = this.top - 5;
+                    break;
+                case 'bottom':
+                    this.top = this.top + 5;
+                    break;
+            }
+            
+            this.bullet.style.left = this.left + 'px';
+            this.bullet.style.top = this.top + 'px';
+            this.parent.bulletRun(this);
+
+            if (this.left < - 100 
+                || this.left > wrap.offsetWidth
+                || this.top < - 100
+                || this.top > wrap.offsetHeight
+            ) {
+                this.remove();
+            }
+        }, 16);
+    }
+
+    remove () {
+        clearInterval(this.run);
+        this.bullet.remove();
+        this.isDestoryed = true;
+
+        let bulletLength = this.parent.bulletArray.filter(item => !item.isDestoryed).length;
+        if (!bulletLength && this.parent.water === 0) {
+            console.log('你输了！');
+        }
+    }
+}
---
+++ src/water.js
@@ -0,0 +1,51 @@
+import Bullet from './bullet'
+export default class Water {
+    constructor (parent) {
+        this.width = 100;
+        this.height = 100;
+        this.parent = parent;
+        this.wrap = parent.wrap;
+        this.level = Math.floor(Math.random() * 5);
+
+        const div = document.createElement('div');
+        div.classList.add('water-polo');
+        const img = document.createElement('img');
+        img.src = `src/img/${this.level}.png`;
+        this.img = img;
+        div.appendChild(img);
+        this.wrap.appendChild(div);
+
+        this.left = div.offsetLeft;
+        this.top = div.offsetTop;
+
+        div.onclick = () => {
+            if (this.level === 0 || this.parent.water === 0) {
+                return;
+            }
+            this.parent.update(this);
+            this.update();
+        }
+
+        this.img.addEventListener('animationend', () => {
+            this.img.classList.remove('level-up');
+        })
+    }
+
+    update () {
+        if (this.level === 0) {
+            return;
+        }
+
+        if (this.level === 4) {
+            this.parent.bulletArray.push(new Bullet(this.parent, this, 'left'));
+            this.parent.bulletArray.push(new Bullet(this.parent, this, 'right'));
+            this.parent.bulletArray.push(new Bullet(this.parent, this, 'top'));
+            this.parent.bulletArray.push(new Bullet(this.parent, this, 'bottom'));
+            this.level = 0;
+        } else {
+            this.level++;
+        }
+        this.img.src = `src/img/${this.level}.png`;
+        this.img.classList.add('level-up');
+    }
+} 
